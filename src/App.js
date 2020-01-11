9ca0bacfaa4c950c4666f58bcc3b3ced0e011b49
Pooja Chawla <pooja@Poojas-MacBook-Air.local>
2020-01-12 02:12:20 +0530

    added cart-icon and cart-dropdown components and implemented cart reducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
@@ -41,7 +41,7 @@ class App extends React.Component {
         <Switch>
         	<Route exact path='/' component={HomePage}/>
         	<Route path='/shop' component={ShopPage}/>
-        	<Route path='/signin' component={SignInAndSignUpPage}/>
+        	<Route exact path='/signin' render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignInAndSignUpPage/>)}/>
         </Switch>
         </div>
 
@@ -49,7 +49,16 @@ class App extends React.Component {
     }
 }
 
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
-    setCurrentUser:user=>dispatch(setCurrentUser(user))
-})
-export default connect(null, mapDispatchToProps)(App);
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
